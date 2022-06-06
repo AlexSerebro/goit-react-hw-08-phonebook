e3a535a58a4257b174f89d447272d5059dd1c89e
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-06-06 19:14:15 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import phonebookReducer from './reducer';
 
-const store = createStore(phonebookReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = configureStore({
+  reducer: phonebookReducer,
+}
 );
 
 
