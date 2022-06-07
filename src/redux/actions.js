85c9c3369f67d5aa912d6a570b7175a58d25b822
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-06-07 21:04:38 +0300

    add actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,30 +1,9 @@
-import constants from "./constants"
+import { createAction } from '@reduxjs/toolkit';
 
-export const setFilterValue = (value) => {
-  return {
-    type: constants.SET_FILTER,
-    payload: value,
-  }
-};
+export const setFilterValue = createAction('SET_FILTER');
 
-export const addContact = (contact) => {
-  return {
-    type: constants.ADD_CONTACT,
-    payload: contact,
-  }
-}
+export const addContact = createAction('ADD_CONTACT');
 
-export const delContact = (id) => {
-  return {
-    type: constants.DEL_CONTACT,
-    payload: id,
-  }
-}
+export const delContact = createAction('DEL_CONTACT');
 
-export const updContacts = (contacts) => {
-  return {
-    type: constants.UPD_CONTACTS,
-    payload: contacts,
-  }
-}
-
+export const updContacts = createAction('UPD_CONTACTS');
