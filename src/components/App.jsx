6604b32e4e0b7cc5d3ed412033cc0bb04fee1d86
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-06-25 12:45:39 +0300

    Add LoginView

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import { PhoneBook } from './PhoneBook';
 import Container from './Container/Container';
 import { AppBar } from './AppBar';
 import { HomeView } from 'views/HomeView/HomeView';
+import { LoginView } from 'views/LoginView/LoginView';
 
 export const App = () => {
   return (
@@ -14,7 +15,7 @@ export const App = () => {
         <Routes>
           <Route exact="true" path="/" element={<HomeView />} />
           <Route path="phonebook" element={<PhoneBook />} />
-          {/* <Route path="login" element={<Login />} /> */}
+          <Route path="login" element={<LoginView />} />
         </Routes>
       </Suspense>
     </Container>
---
+++ src/views/LoginView/LoginView.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+// import { authOperations } from '../redux/auth';
+import s from './Login.module.css';
+
+export function LoginView() {
+  // const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // const handleChange = ({ target: { name, value } }) => {
+  //   switch (name) {
+  //     case 'email':
+  //       return setEmail(value);
+  //     case 'password':
+  //       return setPassword(value);
+  //     default:
+  //       return;
+  //   }
+  // };
+
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   dispatch(authOperations.logIn({ email, password }));
+  //   setEmail('');
+  //   setPassword('');
+  // };
+
+  return (
+    <div>
+      <h1>Login page</h1>
+
+      <form onSubmit={() => {}} className={s.form} autoComplete="off">
+        <label className={s.label}>
+          Email
+          <input type="email" name="email" value={email} onChange={() => {}} />
+        </label>
+
+        <label className={s.label}>
+          Password
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={() => {}}
+          />
+        </label>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
