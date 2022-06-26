41be5502fa1befc927347da8bc19789b380e3a80
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-06-26 10:43:16 +0300

    add isloggedin on phonebook

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,29 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getIsLoggedIn } from 'redux/auth/authSelectors';
 import s from './Navigation.module.css';
 
-export const Navigation = () => (
-  <nav>
-    <NavLink
-      end
-      exact="true"
-      to="/"
-      className={({ isActive }) => (isActive ? s.link_active : s.link)}
-    >
-      Home
-    </NavLink>
+export const Navigation = () => {
+  const isLoggedIn = useSelector(getIsLoggedIn);
+  return (
+    <nav>
+      <NavLink
+        end
+        exact="true"
+        to="/"
+        className={({ isActive }) => (isActive ? s.link_active : s.link)}
+      >
+        Home
+      </NavLink>
 
-    <NavLink
-      to="/contacts"
-      className={({ isActive }) => (isActive ? s.link_active : s.link)}
-    >
-      Phonebook
-    </NavLink>
-  </nav>
-);
+      {isLoggedIn && (
+        <NavLink
+          to="/contacts"
+          className={({ isActive }) => (isActive ? s.link_active : s.link)}
+        >
+          Phonebook
+        </NavLink>
+      )}
+    </nav>
+  );
+};
