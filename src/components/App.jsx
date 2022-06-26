742c23a3f4a4a7efcda7cd5de9d394a23bb91af4
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-06-26 11:09:46 +0300

    fix flesh content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Suspense, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import { PhoneBook } from './PhoneBook';
@@ -10,47 +11,52 @@ import RegisterView from 'views/RegisterView/RegisterView';
 import { fetchCurrentUser } from '../redux/auth/authOperations';
 import { PrivateRoute } from './UserMenu/PrivateRoute';
 import { PublicRoute } from './UserMenu/PublicRoute';
+import { getIsFetchingCurrent } from '../redux/auth/authSelectors';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(getIsFetchingCurrent);
+  // console.log(isFetchingCurrentUser);
 
   useEffect(() => {
     dispatch(fetchCurrentUser());
   }, [dispatch]);
 
   return (
-    <Container>
-      <AppBar />
+    !isFetchingCurrentUser && (
+      <Container>
+        <AppBar />
 
-      <Suspense fallback={<p>Loading</p>}>
-        <Routes>
-          <Route path="/" element={<HomeView />} />
-          <Route
-            path="contacts"
-            element={
-              <PrivateRoute>
-                <PhoneBook />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="login"
-            element={
-              <PublicRoute redirectTo="/contacts">
-                <LoginView />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="register"
-            element={
-              <PublicRoute redirectTo="/">
-                <RegisterView />
-              </PublicRoute>
-            }
-          />
-        </Routes>
-      </Suspense>
-    </Container>
+        <Suspense fallback={<p>Loading</p>}>
+          <Routes>
+            <Route path="/" element={<HomeView />} />
+            <Route
+              path="contacts"
+              element={
+                <PrivateRoute>
+                  <PhoneBook />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <PublicRoute redirectTo="/contacts">
+                  <LoginView />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="register"
+              element={
+                <PublicRoute redirectTo="/">
+                  <RegisterView />
+                </PublicRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
+      </Container>
+    )
   );
 };
